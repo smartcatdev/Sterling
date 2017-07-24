41de3d75756a65b4e0b527eb2480d588619f0978
Rylee A. Vereecken <RAVereecken@gmail.com>
2017-07-24 12:17:57 -0400

    navbar fixed on single page

--- single-post.php
+++ single-post.php
@@ -8,72 +8,78 @@ get_header(); ?>
                         
                         <div class="row">
                             
-                            <div id="single-post" class="col-md-8">
+                            <div id="single-post" class="col-md-9">
                         
                                 <?php while ( have_posts() ) : the_post(); ?>
                                 
-                                <div id="single-post-title">
-                                    
-                                    <div id="single-post-title-img" style="background: url(<?php echo get_the_post_thumbnail_url(); ?>) center;">
-                                        <?php echo get_the_post_thumbnail(); ?>
+                                    <div id="single-post-title">
+
+                                        <div id="single-post-title-img" style="background: url(<?php echo get_the_post_thumbnail_url(); ?>) center;">
+                                            <?php echo get_the_post_thumbnail(); ?>
+                                        </div>
+
+                                        <div id="single-post-title-text">
+
+                                            <h4><?php echo get_the_title(); ?></h4>
+
+                                            <a href="<?php echo get_the_author_meta('user_url'); ?>">
+                                                By <?php echo get_the_author_meta('display_name'); ?>
+                                            </a>
+
+                                            <span></span>
+
+                                            <p><?php echo get_the_date('m/d/Y'); ?></p>
+
+                                            <span></span>
+
+                                            <a href="<?php echo get_comment_link(); ?>">
+                                                <?php echo get_comments_number() . ' '; ?>Comments
+                                            </a>
+
+                                        </div>
+
                                     </div>
-                                    
-                                    <div id="single-post-title-text">
+                                    <div id="single-post-content">
 
-                                        <h4><?php echo get_the_title(); ?></h4>
-
-                                        <a href="<?php echo get_the_author_meta('user_url'); ?>">
-                                            By <?php echo get_the_author_meta('display_name'); ?>
-                                        </a>
-
-                                        <span></span>
-
-                                        <p><?php echo get_the_date('m/d/Y'); ?></p>
-
-                                        <span></span>
-
-                                        <a href="<?php echo get_comment_link(); ?>">
-                                            <?php echo get_comments_number() . ' '; ?>Comments
-                                        </a>
+                                        <?php echo get_the_content(); ?>
 
                                     </div>
-                                    
-                                </div>
-                                <div id="single-post-content">
-                                    
-                                    <?php echo get_the_content(); ?>
-                                    
-                                </div>
-                                
-                                <span id="content-divider"></span>
-                                 
-                                <div id="single-post-tags">
-                                    
-                                    <?php $tags = get_the_tags(); ?> 
-                                    
-                                    Tags: 
-                                    
-                                    <?php foreach ( $tags as $tag ) : ?>
 
-                                        <a class="btn btn-warning"
-                                           href="<?php bloginfo('url');?>/tag/<?php echo ($tag->slug);?>">
-                                                 <?php echo ($tag->name); ?>
-                                        </a> 
+                                    <span id="content-divider"></span>
 
-                                    <?php endforeach; ?>
-                                    
-                                </div>
+                                    <div id="single-post-tags">
 
-                                <?php // If comments are open or we have at least one comment, load up the comment template.
-                                if ( comments_open() || get_comments_number() ) :
-                                        comments_template();
-                                endif;
+                                        <?php $tags = get_the_tags(); ?> 
+
+                                        Tags: 
+
+                                        <?php foreach ( $tags as $tag ) : ?>
+
+                                            <a class="btn btn-warning"
+                                               href="<?php bloginfo('url');?>/tag/<?php echo ($tag->slug);?>">
+                                                     <?php echo ($tag->name); ?>
+                                            </a> 
+
+                                        <?php endforeach; ?>
+
+                                    </div>
+
+                                    <div id="single-post-author-info">
+                                        <div id="single-post-author-img" style="background: url(<?php echo ltrim(get_avatar_url(the_author_meta('ID'), 64), '1'); ?>)">
+
+                                        </div>
+                                    </div>
+
+                                    <?php // If comments are open or we have at least one comment, load up the comment template.
+                                    if ( comments_open() || get_comments_number() ) :
+                                            comments_template();
+                                    endif;
 
                                 endwhile; // End of the loop. ?>
                         
                             </div>
                             
-                            <div id="single-post-sidebar" class="col-md-4">
+                            <div id="single-post-sidebar" class="col-md-3">
                                 
                                 <?php get_sidebar(); ?>
                                 
